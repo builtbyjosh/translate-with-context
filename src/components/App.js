a55b8f8ecda8f2af5e495860867ab1ba2509ac76
builtbyjosh <pc.refuge@gmail.com>
2021-08-09 21:42:58 -0500

    connected context to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import UserCreate from './UserCreate'
+import LanguageContext from '../contexts/LanguageContext'
+import { language } from 'language-tags'
 
  class App extends Component {
   state = {language: 'english'}
@@ -16,7 +18,10 @@ import UserCreate from './UserCreate'
           <i className="flag us" onClick={() => this.onLanguageChange('english')} />
           <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
+        
       </div>
     )
   }
